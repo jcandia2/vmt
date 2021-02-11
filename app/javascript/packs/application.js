f99b6e9eb0c1fc21c0ccb04ea33546e90261b860
Juan Candia <jcandia2@users.noreply.github.com>
2021-02-10 22:36:27 -0700

    Update: decodes VIN with NHTSA api

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("packs/vehicles")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,5 +16,31 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import 'bootstrap';
-        import './stylesheets/application.scss';
+import './stylesheets/application.scss';
 
+
+document.addEventListener("turbolinks:load", function () {
+  function setVinValues() {
+    $.ajax({
+      url: "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/",
+      type: "POST",
+      data: { format: "json", vin: $('#vehicle_vin').val() },
+      success: function (result) {
+        var resultJson = JSON.parse(result.type_options)["result"]
+        console.log(resultJson)
+        // if (resultJson) {
+        //   $('#vehicle_year').val(resultJson[0]["modelyear"]);
+        //   $('#vehicle_make').val(resultJson[0]["make"]);
+        //   $('#vehicle_model').val(resultJson[0]["model"]);
+        //   $('#vehicle_trim').val(resultJson[0]["body"]);
+        // }
+        // Set any standard vehicle information.
+        // $('#vehicle_uid').replaceOptions(result.type_options);
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+        // console.log(xhr.status);
+        // console.log(thrownError);
+      }
+    });
+  };
+});
---
+++ app/javascript/packs/vehicles.js
@@ -0,0 +1,35 @@
+function setVinValues() {
+  $.ajax({
+    url: "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVINValuesBatch/",
+    type: "POST",
+    data: { format: "json", data: $('#vehicle_vin').val() },
+    dataType: "json",
+    success: function (result) {
+      var vehicleData = result.Results
+      console.log(vehicleData)
+      if (vehicleData.length > 0) {
+        $('#vehicle_year').val(vehicleData[0]["ModelYear"]);
+        $('#vehicle_make').val(vehicleData[0]["Make"]);
+        $('#vehicle_model').val(vehicleData[0]["Model"]);
+        $('#no-data-found').html("");
+      } else {
+        $('#no-data-found').append('No data found for this Vin');
+      }
+    },
+    error: function (xhr, ajaxOptions, thrownError) {
+      console.log(xhr.status);
+      console.log(thrownError);
+    }
+  });
+};
+
+$(document).on("turbolinks:load", () => {
+  console.log('im here');
+  $('#vin-api-refresh').on("click", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    console.log('clicked');
+    setVinValues();
+  });
+});
+
